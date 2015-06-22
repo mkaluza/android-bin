1132d1e83b49c0d9b1c48fb77fcc930452403d0d
Marcin Kaluza <marcin.kaluza@trioptimum.com>
2015-06-22 21:59:53 +0200

    smarter uv tester

--- extreme_uv.sh
+++ extreme_uv.sh
@@ -2,7 +2,9 @@
 
 #set -x
 
-which bc > /dev/null
+#which bc > /dev/null
+BC=/data/bin/bc
+
 if [ $? -gt 0 ]
 then
 	cat << _EOF
@@ -15,26 +17,19 @@ _EOF
 	exit 1
 fi
 
-gov=`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor`
+echo userspace > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 
-if [ "$gov" != "userspace" -a "$gov" != "performance" -a "$gov" != "powersave" ]
-then
-	echo "Please change governor to userspace, performance or powersave (it's $gov now) and set desired frequency"
-	exit 1
-fi
-
-REG=`echo $1 | tr a-f A-F`
-
-if [ "$REG" != "0xB" -a "$REG" != "0xC" ]
-then
-	cat << _EOF
-usage: `basename $0` REG
-where REG is 0xB for ARM_100_OPP and ARM_MAX_OPP or 0xC for ARM_50_OPP and ARM_EXT_CLK
-_EOF
-	exit 1
-fi
-
+[ -n "$1" ] && echo ${1}000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed
 freq=`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq`
+
+step=`grep -l -e "[^0-9]$freq kHz" /sys/kernel/liveopp/arm_step*`
+opp=`grep ArmOPP $step | awk '{print $2}'`
+
+if [ $opp == "ARM_EXTCLK" -o "$opp" == "ARM_50_OPP" ]; then
+	REG="0x0C"
+else
+	REG="0x0B"
+fi
 
 echo "Testing extreme undervolt for $freq with $REG register"
 
@@ -42,15 +37,19 @@ cd /sys/kernel/debug/ab8500/
 echo 4 > register-bank
 echo $REG > register-address
 
-start=`cat register-value | tr a-f A-F | sed -e "s/0x//g"`
-start=`echo "ibase=16; $start-1" | bc`
-delay=60
+start=$2
+[ -z "$start" ] && start=`cat register-value `
+start=`echo $start | tr a-f A-F | sed -e "s/0x//g"`
+
+start=`echo "ibase=16; $start-1" | $BC`
+[ -z "$delay" ] && delay=60
 for V in `seq $start -1 0`
-	do Vh=`echo "obase=16; $V" | bc`
+	do Vh=`echo "obase=16; $V" | $BC`
 	echo "Setting 0x$Vh"
 	echo "0x$Vh" > register-value
 	for i in `seq $delay -1 1`; do
 		echo -n ". "
 		sleep 1;
 	done
+	echo
 done
